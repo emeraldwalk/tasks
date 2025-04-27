e00fc62d5244a505bef343411399597b12c0a1ab
Brian Ingles <github@emeraldwalk.com>
2025-04-27 18:19:50 -0500

    chapter accordion

--- web/src/components/Chapter.tsx
+++ web/src/components/Chapter.tsx
@@ -1,5 +1,7 @@
-import { createEffect, createSignal } from 'solid-js'
+import { createSignal } from 'solid-js'
 import styles from './Chapter.module.css'
+import { className } from '../utils/cssUtils'
+import accordionStyles from './Accordion.module.css'
 
 export interface ChapterProps {
   bookName: string
@@ -9,14 +11,23 @@ export interface ChapterProps {
 }
 
 export function Chapter(props: ChapterProps) {
+  const [isExpanded, setIsExpanded] = createSignal(false)
+
   return (
-    <div class={styles.Chapter}>
-      <span class={styles.label}>
+    <div
+      class={className(
+        styles.Chapter,
+        isExpanded() && styles.isExpanded,
+        isExpanded() && accordionStyles.isExpanded,
+      )}>
+      <span class={styles.label} onClick={() => setIsExpanded((prev) => !prev)}>
         {props.bookName} {props.number}
-        <ion-icon name="chevron-down-sharp"></ion-icon>
+        <ion-icon
+          class={accordionStyles.icon}
+          name="chevron-down-sharp"></ion-icon>
       </span>
       <button>+</button>
-      <div class={styles.dateList}>
+      <div class={className(styles.dateList, accordionStyles.content)}>
         <button class={styles.date}>04/26</button>
         <button class={styles.date}>04/24</button>
         <button class={styles.date}>04/26</button>
